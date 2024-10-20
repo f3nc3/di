dca7819017fd3d6d9786d271caa6498b1fcd5435
f3nc3 <168217860+f3nc3@users.noreply.github.com>
2024-10-21 00:34:27 +0530

    Add files via upload

---
+++ GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.simple;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet(@RequestParam String name) {
+        return greetingService.greet(name);
+    }
+}
+
+//mvn spring-boot:run
+//http://localhost:8080/greet?name=John
---
+++ GreetingService.java
@@ -0,0 +1,5 @@
+package com.example.simple;
+
+public interface GreetingService {
+    String greet(String name);
+}
